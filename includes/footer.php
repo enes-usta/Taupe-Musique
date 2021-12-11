59c84f269deb2f961d098b035e5c1871ff7b2365
solsdii <sahnoune.hugoo@gmail.com>
2021-12-11 17:12:03 +0100

    Correction footer indice

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<div class="row">
 		<div class="col-lg-12">
-            <p><FONT color="white">Connecter la page </FONT></p>
+            <p><FONT color="white">Enlever la partie documentation 127.0.0.1:8081 </FONT></p>
 			<p>Copyright © 2021</p>
 		</div>
 	</div>
