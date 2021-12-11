6f72377fa1d19ab77785962c1daa4e94f7f1847a
Enes <mine.skysylver@gmail.com>
2021-12-11 19:03:14 +0100

    Login
    Auth BDD

--- linuxdocumentation/SearchProduct.php
+++ linuxdocumentation/SearchProduct.php
@@ -1,36 +1,24 @@
 <?php
 session_start();
-/*include '../Admin/authorized.php';*/
-include_once 'Database/DB.php';
-
-//echo "ca a marchÃ©";
-
+include 'Parametres.php';
 
 $user = $_POST["user"];
 
 if ($user != "") {
-    $mysqli=mysqli_connect("127.0.0.1","root","","cds") or die("Problème de création de la base :".mysqli_error());
-    //echo "$mysqli";
-    //echo "$user";
+    global $host, $user, $password, $database;
+    $mysqli=mysqli_connect($host,$user,$password,$database) or die("Problème de création de la base :".mysqli_error());
+
     $req = mysqli_query($mysqli, "SELECT * FROM users WHERE NOM = '$user'");
     if($req != false){
         $cpt = 0;
         while($data = mysqli_fetch_array($req)){
             $cpt ++;
-            echo '<a href="fiche.php">'.$data['NOM'].' '.$data['PRENOM'].'</a>';
-            echo "<br/>";
+            echo '<a href="fiche.php">'.$data['NOM'].' '.$data['PRENOM'].'</a><br/>';
         }
-        if($cpt ==0){
-            echo 'aucun résultat';
-            echo"<br/>";
-        }
+        if($cpt ==0)
+            echo 'aucun résultat<br/>';
     }
     echo"<br/>";
-
-
-   //printf("error: %s\n", mysqli_error($mysqli));
-   //echo " res = $req";
-
 }
 ?>
 
