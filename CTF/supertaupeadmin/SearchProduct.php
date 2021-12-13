50ba4f2b2b5cfb65d3f59d8df7f302a392b25f36
Enes <mine.skysylver@gmail.com>
2021-12-13 22:08:17 +0100

    CTF filenames & ...

---
+++ CTF/supertaupeadmin/SearchProduct.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include 'Parametres.php';
+
+$sent_user = $_POST["user"];
+
+if ($sent_user != "") {
+    global $host, $user, $password, $database;
+    $mysqli = mysqli_connect($host, $user, $password, $database);// or die("Problème de création de la base :".mysqli_error());
+
+    $req = mysqli_query($mysqli, "SELECT * FROM user WHERE username = '$sent_user'");
+    if ($req != false) {
+        $cpt = 0;
+        ?>
+        <table>
+            <tr>
+                <th scope="col">Username</th>
+                <th scope="col">Password</th>
+                <th scope="col">Email</th>
+            </tr>
+        </table>
+        <?php
+        while ($data = mysqli_fetch_array($req)) {
+            $cpt++;
+            ?>
+            <tr>
+                <td align=center><?= $data['username'] ?></td>
+                <td align=center><?= $data['password'] ?></td>
+                <td align=center><?= $data['email'] ?></td>
+            </tr>
+            <?php
+        }
+
+        echo '</table>' . ($cpt == 0 ? 'Aucun résultat<br/>' : '<br/>');
+    }
+}
+?>
+
+<input type=button onclick=window.location.href="RechercheBarre.php'; value=retour/>
